5d67abb6004be65e120d7f4d390bf959d0f7d791
Bartosz <bartosz.lewicki@gmail.com>
2019-02-03 17:17:04 +0100

    Mobile version

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-
 gulp.task('pranie', function(done) {
     console.log('robię pranie');
     done();
 });
 
+
+
 gulp.task('sass', function() {
-  return gulp.src('scss/main.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      errLogToConsole: true,
-      outputStyle: 'expanded',
-      sourceComments: 'map'
+    return gulp.src('scss/main.scss')
+            .pipe(sourcemaps.init())
+            .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: 'expanded',
+            sourceComments: 'map'
     }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('css'))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('css'))
 });
 
 gulp.task('watch', function() {
-  gulp.watch('scss/*.scss', gulp.series('sass'));
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
 });
+
